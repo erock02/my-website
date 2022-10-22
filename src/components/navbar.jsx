fc38b90c1f31dd4fa3208ddb811c93e8c8b27707
Eric Espindola <eric_e_2012@hotmail.com>
2022-10-22 03:00:24 -0700

    refactor to add navbar and highlight active link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,60 @@
 import React, { Component } from "react";
-
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "../pages/home";
+import CV from "../pages/cv";
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Eric Espindola Chavez
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-              <a className="nav-link" href="/resume">
-                Resume
-              </a>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              Eric Espindola Chavez
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <li className="nav-link">
+                  <NavLink
+                    end
+                    to="/"
+                    style={({ isActive }) => ({
+                      color: isActive ? "blue" : "black",
+                    })}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-link">
+                  <NavLink
+                    to="resume"
+                    exact="false"
+                    style={({ isActive }) => ({
+                      color: isActive ? "blue" : "black",
+                    })}
+                  >
+                    Resume
+                  </NavLink>
+                </li>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="resume" element={<CV />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
