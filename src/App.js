b99e38c0156ea7a10093a6cd5a17a60eb663e325
Eric Espindola <eric_e_2012@hotmail.com>
2022-10-25 04:57:23 -0700

    commenting out footer import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavBar from "./components/navbar";
 import "./App.css";
 import beach from "./beach.jpg";
-import Footer from "./components/footer";
+// import Footer from "./components/footer";
 
 function App() {
   return (
