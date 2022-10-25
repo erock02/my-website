6e96780900ab85b3cb47daabbe2c7ffe13880386
Eric Espindola <eric_e_2012@hotmail.com>
2022-10-25 04:12:43 -0700

    add footer to app. Set background image to center

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import NavBar from "./components/navbar";
 import "./App.css";
 import beach from "./beach.jpg";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage: `url(${beach})`, backgroundRepeat: "false", backgroundSize: "cover"}}>
+    <div className="App" style={
+      {backgroundImage: `url(${beach})`, 
+      backgroundRepeat: "false", 
+      backgroundSize: "cover", 
+      backgroundPosition: "center"}}
+      >
       <div className="NavBar">
         <NavBar />
+        <Footer />
       </div>
     </div>
   );
